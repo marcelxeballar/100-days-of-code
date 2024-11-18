fc302e6959b8d006d5cb51e24eb2d148aa919a39
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-11-18 01:54:03 -0400

    build: create image selection component in React

--- 02-add-to-bag/src/App.js
+++ 02-add-to-bag/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import backIMG from "./assets/back.jpg";
+import frontIMG from "./assets/front.jpg";
+import sideIMG from "./assets/side.jpg";
 
-function App() {
+const size = ["S", "M", "L", "XL", "XXL"];
+const imgSRC = [frontIMG, sideIMG, backIMG];
+export default function App() {
+  const [isDisplayed, setIsDisplayed] = useState(backIMG);
+  const [sizeActive, setSizeActive] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className="item-img-wrapper flex-row">
+        <div className="item-view-wrapper flex-col">
+          {imgSRC.map((src, i) => (
+            <img
+              className={`${isDisplayed === src ? "active" : ""}`}
+              src={src}
+              key={i}
+              onClick={() => setIsDisplayed(src)}
+              alt={`img-${i}`}
+            />
+          ))}
+        </div>
+        <div className="current-view">
+          <img src={isDisplayed} alt="Item Back" />
+        </div>
+      </div>
+      <div className="item-info-wrapper">
+        <p className="brand">POLO RAPLH</p>
+        <h2 className="item-name">Custom Fit Polo Bear Oxford Shirt</h2>
+        <p className="item-info">
+          Blue polo with a classic cut. Made of smooth and soft cotton.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="price-wrapper flex-col">
+          <div className="new-price flex-row">
+            <h2>$99.00</h2>
+            <p>-25%</p>
+          </div>
+          <p>$132.00</p>
+        </div>
+        <div className="size-wrapper">
+          <p>CHOOSE SIZE</p>
+          <div className="choose-size flex-row">
+            {size.map((item, i) => (
+              <p
+                className={`flex-row ${sizeActive === i ? "size-active" : ""}`}
+                key={i}
+                onClick={() => setSizeActive(i)}
+              >
+                {item}
+              </p>
+            ))}
+          </div>
+        </div>
+        <button className="atc-btn">ADD TO CART</button>
+      </div>
+    </>
   );
 }
-
-export default App;
