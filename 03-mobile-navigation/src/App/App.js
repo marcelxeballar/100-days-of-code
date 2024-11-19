bae66184b82b4178f31cf56b21fa76d8e371e3c7
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-11-19 14:47:05 -0400

    feat: created React-based navigation

--- 03-mobile-navigation/src/App/App.js
+++ 03-mobile-navigation/src/App/App.js
@@ -1,5 +1,56 @@
-function App() {
-  return <></>;
-}
+import "./app.css";
+import profileIMG from "../Assets/profile.png";
 
-export default App;
+const icons = ["person", "mail", "notifications", "location_on", "groups"];
+const messages = [
+  "Personal Data",
+  "Messages",
+  "Notifications",
+  "Location",
+  "Community",
+];
+
+const navIcons = ["home", "bar_chart", "mail", "person"];
+export default function App() {
+  return (
+    <div className="navigation">
+      <div className="flex-row profile">
+        <img src={profileIMG} alt="Profile" />
+        <div className="profile-info flex-col">
+          <h2>Lisa Richardson</h2>
+          <p>Environmental meteorologist</p>
+        </div>
+      </div>
+      <div className="flex-col actions">
+        {messages.map((message, i) => (
+          <div className="flex-row action">
+            <span class="material-icons">{icons[i]}</span>
+            <h2>{message}</h2>
+            <span class="material-icons">chevron_right</span>
+          </div>
+        ))}
+      </div>
+      <div className="flex-col actions">
+        <div className="flex-row action">
+          <span class="material-icons">help</span>
+          <h2>FAQ's</h2>
+          <span class="material-icons">chevron_right</span>
+        </div>
+        <div className="flex-row action">
+          <span class="material-icons">settings</span>
+          <h2>FAQ's</h2>
+          <span class="material-icons">chevron_right</span>
+        </div>
+      </div>
+      <div className="navigation-bar flex-row">
+        {navIcons.map((icon, i) => (
+          <button key={i} className="flex-row">
+            <span class={`material-icons ${icon === "person" ? "active" : ""}`}>
+              {icon}
+            </span>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
